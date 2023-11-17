6576f0264a301f43457e71bbb82118ebebcded41
oliullah05 <mdolihasan@gmail.com>
2023-11-18 02:57:51 +0600

    global error and global router error show

--- dist/app/app.js
+++ dist/app/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -36,15 +45,41 @@ courseRouter.post("/create-course", (req, res) => {
         data: course
     });
 });
-app.get('/', logger, (req, res) => {
-    console.log(req.query);
-    // console.log(req.params);
-    res.send('Hello Developers222!');
-});
-app.post('/', logger, (req, res) => {
+app.get('/', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        // Intentionally using an undefined variable to cause an error
+        res.send(something);
+    }
+    catch (err) {
+        console.log(err);
+        next(err);
+        // res.status(404).json({
+        //   success: false,
+        //   message: "fail to get data"
+        // })
+    }
+    // res.send('Hello Developers222!')
+}));
+app.post('/route2', logger, (req, res) => {
     console.log(req.body);
     res.json({
         "message": "sueccesfully get data"
     });
+});
+// global route error show
+app.all("*", (req, res) => {
+    res.status(400).json({
+        success: false,
+        messange: "you enter wrong route"
+    });
+});
+// global error handler 
+app.use((error, req, res, next) => {
+    if (error) {
+        res.status(400).json({
+            success: false,
+            message: "something went erong"
+        });
+    }
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -7,40 +7,68 @@ app.use(express.json())
 app.use(express.text())
 
 //middle wars
-const logger = (req:Request,res:Response,next:NextFunction)=>{
-  console.log(req.url,req.method,req.hostname);
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.method, req.hostname);
   next()
 }
 
 
 
-
 const userRouter = express.Router();
-app.use('/api/v1/users',userRouter)
-userRouter.get("/create-user",(req:Request,res:Response)=>{
-    const userData = req.body;
-    console.log(userData);
-    res.json({
-      success:true,
-      message:"user is created successfully",
-      data:userData
-    })
+app.use('/api/v1/users', userRouter)
+userRouter.get("/create-user", (req: Request, res: Response) => {
+  const userData = req.body;
+  console.log(userData);
+  res.json({
+    success: true,
+    message: "user is created successfully",
+    data: userData
+  })
+})
+
+
+const courseRouter = express.Router();
+app.use("/api/v1/courses", courseRouter)
+courseRouter.get("/", courseRouter)
+
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+  const course = req.body;
+  console.log(course);
+  res.json({
+    success: true,
+    message: "course is created successfully",
+    data: course
+  })
 })
 
 
 
+app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // Intentionally using an undefined variable to cause an error
+    res.send(something)
+  }
+  catch (err) {
+    console.log(err);
+    next(err)
+    // res.status(404).json({
+    //   success: false,
+    //   message: "fail to get data"
+    // })
+  }
 
-const courseRouter = express.Router();
-app.use("/api/v1/courses",courseRouter)
-courseRouter.get("/",courseRouter)
 
-courseRouter.post("/create-course",(req:Request,res:Response)=>{
-  const course = req.body;
-  console.log(course);
+
+
+
+  // res.send('Hello Developers222!')
+})
+
+
+app.post('/route2', logger, (req: Request, res: Response) => {
+  console.log(req.body);
   res.json({
-    success:true,
-    message:"course is created successfully",
-    data:course
+    "message": "sueccesfully get data"
   })
 })
 
@@ -49,31 +77,26 @@ courseRouter.post("/create-course",(req:Request,res:Response)=>{
 
 
 
+// global route error show
 
-
-
-app.get('/', logger,(req:Request, res:Response) => {
-  console.log(req.query);
-  // console.log(req.params);
-  res.send('Hello Developers222!')
-})
-
-app.post('/',logger,(req:Request,res:Response)=>{
-    console.log(req.body);
-    res.json({
-      "message":"sueccesfully get data"
-    })
+app.all("*",(req:Request,res:Response)=>{
+  res.status(400).json({
+    success:false,
+    messange:"you enter wrong route"
+  })
 })
 
 
 
 
-
-
-
-
-
-
-
+// global error handler 
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+ if(error){
+  res.status(400).json({
+    success:false,
+    message:"something went erong"
+  })
+ }
+})
 
 export default app;
